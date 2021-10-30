1880a916f644ac790bfad866b77fa1b1435d354d
kkyun8 <kim23302@icloud.com>
2021-10-30 22:30:18 +0900

    helloworld成功

--- handler.js
+++ handler.js
@@ -16,3 +16,17 @@ module.exports.hello = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.helloHoge = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin' : '*'
+    },
+    body: JSON.stringify({
+      message: `Hello ${event.pathParameters.name}!`,
+    }),
+  };
+
+  callback(null, response);
+};
